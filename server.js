5fced286541ace997264ef74c9cf00d085341340
Artur Moreira dos Santos <arturmoreira56dossantos@gmail.com>
2024-11-21 18:06:47 -0300

    Criação total de APi

---
+++ server.js
@@ -0,0 +1,10 @@
+import expres from 'express';
+
+const app = expres();
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
+});
+
+app.get("/api", (req, res) => {
+    res.status(200).send("Boas vindas a imersão!");
+})
